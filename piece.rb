98e7f50579ffbc73d4ac964d7265ac3b0a37553d
App Academy Student <student@appacademy.io>
2014-02-04 13:40:15 -0800

    Add simplified Pawn

--- piece.rb
+++ piece.rb
@@ -24,7 +24,41 @@ end
 
 
 class Pawn < Piece
-  #You're crazy, figure out later
 
+  #TODO: depending on game state and whether it's claiming a piece it's moves are different
+  def initialize(color, pos, board=nil)
+    super(color,pos,board)
+    @move_units = [
+      [1,1],
+      [-1,1],
+      [0,1]
+    ]
+
+  end
+
+  def possible_moves
+    valid_moves = []
+    direction = self.direction
+    x, y = self.position
+
+    @move_units.each do |(dx, dy)|
+      new_position = [x + (direction * dx), y + (direction * dy)]
+      if new_position[0].between?(0,7) && new_position[1].between?(0,7)
+        # not valid if new position is already occupied (ask self.board)
+        valid_moves << new_position
+      end
+    end
+
+    valid_moves
+  end
+
+  def direction
+    direction = 0
+    if @color == :b
+      direction = -1
+    else
+      direction = 1
+    end
+  end
 end
 
